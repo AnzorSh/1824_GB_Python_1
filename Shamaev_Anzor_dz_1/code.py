e6f6a5ffbdd9ec114d753f678a870872be2609b4
AnzorSh <anzor_sh@ro.ru>
2021-10-07 07:11:36 +0300

    lesson_1

---
+++ Shamaev_Anzor_dz_1/code.py
@@ -0,0 +1,17 @@
+# user_name = input('введите имя: ')  # Иван
+# print('Добрый вечер, ', user_name, '!')  # Добрый вечер,  Иван !
+
+print(5 ** 2)  # 25
+x = 2
+y = x ** 3
+print(y)  # 8
+
+a = 17 % 5
+b = 5 % 5
+c = 3 % 5
+d = 0 % 5
+print(a)  # 2
+print(b)  # 0
+print(c)  # 3
+print(d)  # 0
+
